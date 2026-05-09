166c2f1e1837a9687eabeebb78c22c9792802d4d
muhammedharman-sudo <muhammed_harman@hotmail.com>
2026-05-09 06:46:36 +0300

    Add files via upload

---
+++ gürger.js
@@ -0,0 +1,53 @@
+{
+  "sure_no": 78,
+  "sure_adi": "Nebe",
+  "ayetler": {
+    "1": "1. ayette geçen "Sad" Huruf-u mukattaadandır, Bakara suresinin girişinde 
+(2/1) bu konuda geniş bilgi verilmiştir. "Zikir dolu Kuran öğüdü bol, yol 
+göstermesi doğru, hükümleri isabetli, hikmetli, şanı ve şerefi yüksek Kur'an 
+demektir (bkz. 21/Enbiya, 10). Bu, Kur'an ın tümüne olduğu gibi bir bölümü
+ne de atıf olabilir. 
+Tuhaf olan şu ki, Yüce Allah rahmetinin sonucu olarak insanlara böyle bir 
+kitap indirmişken, vahiy gerçeğine inanmayanlar boş gururlara kapılıp kibir 
+göstermekte, hakka ve hakikate muhalefet içindedirler; bu da onları hakikat-
+ten, iyilik ve doğruluktan uzağa düşürmektedir (bkz. 2/Bakara, 206). Tepeden 
+bakmaları, kendilerini üstte görmeleri içi boş bir gurura kapıldıklarını göste
+rir. Esası itibariyle gurur -garardan aldanmadır. Gurura kapılan kişi aslında 
+kendini aldatmakta, kandırmaktadır. İnsan yaratılmışken, Allah'a muhtaç iken 
+Yüce Allah'ın yol göstermesine, hidayetine karşı üstünlük taslaması, gururu 
+dolayısıyla teslim olmaması boş bir tutumdur. Bu da onu hakikatten ve doğru 
+yoldan uzağa düşürmektedir. 
+"Şikakın parçalanma olma ihtimali de vardır. Bu durumda inkarcılar yani 
+vahyin ilk muhatabı olan putperestler her bir kabile ayrı tanrıya tapınmak su-
+retiyle kendi içlerinde parçalara ayrılmış bulunmaktadırlar. Oysa tevhid inancı 
+onları bir olan Allah'ın iradesi altında toplamakta, birlik ve beraberliğe davet 
+etmektedir. En doğrusunu Allah bilir!",
+    "2": "İ nkarcılar içlerinden birinin -hem beşer nev inden bir insan hem tanıdık-
+ları hemşehrilerinin- elçi olarak seçilip yol gösterici ve uyarıcı sıfatıyla gönde-
+rilmesine şaşırıyorlar. Onların iddialarına göre Tanrı bir elçi gönderecekse bu 
+ya "insanüstü bir varl ık -mesela melek gibi (bk. 6/En am, 8-9; 7 / A raf, 63)­
+veya hiç değilse Mekke ve '{aif gibi iki şehrin ulularından biri olmalıydı ( 43/ 
+Zuhruf, 31). Madem elçi bu iki vasfa değil ve buna rağmen bir takım insanlar 
+ona inanıp etrafında kenetleniyor, olsa olsa büyücü olabilir ancak; büyücüler 
+her ne söylüyorsa yalandır, bir gerçeklik değeri yoktur. 
+İnkarcıların şaştıkları başka bir nokta daha ilginç: Hz. Muhammed (s.a.) 
+çok tanrıcılığı, birden fazla ilaha tapınma adetini ortadan kaldırıp tek Tanrı 
+inancına yani bir olan Allah'a davet ediyordu. Oysa Kabe de her kabilenin bir 
+putu vardı, bunların sayıları 36S'e kadar çıkmıştı. Her kabile kendi özerkliğini, 
+hakimiyet alanını kendine özgü putu ile ifade ediyordu. Her kabilenin putu ol-
+duğundan başka kabilenin nüfuzu veya hakimiyeti altına girmezdi. Bir kabile 
+diğerinin putunu kabul ettiğinde artık zımnen veya açıktan siyasi hakimiyetini, 
+kabile prestij ini ve üstünlüğünü de kabul etmiş, böylelikle başkasının nüfuzu 
+altına girmiş olurdu. Hz. Muhammed (s.a.) onları tek bir Tanrı 'ya davet ettiğin-
+de, onların kabil kültürlerine göre bunun anlamı onun ve dolayısıyla kabilesi 
+veya ailesinin nüfuzu altına girmek demekti. Nitekim Hz. Peygamber'i koruyu-
+cusu olan amcası Ebu Talib'e şikayet ettiklerinde bunu dile getirmişlerdi. Am-
+casının sorusu üzerine de Hz. Peygamber, bu düşüncelerinde yanıldıklarını, 
+eğer çağrısına olumlu cevap vereceklerse Araplar üstünde hakimiyet kurup 
+komşu imparatorlukları vergiye bağlayacaklarını söyleyince Ebu Cehil'in de 
+hazır olduğu meclistekiler hemen atılarak "Söyle fazlasını da verelim! deyin-
+ce Kelim-e i tevhidi söylemelerini buyurdu. Bunun üzerine şiddetle tepki gös-
+terip inkarcı tutumlarına, sövgü ve küfürlerine devam edeceklerini söylediler.",
+    "3": ""
+  }
+}
